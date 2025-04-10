99195af37311f6bd3fc831a4b7bca40d1cc8a623
alexjoo-kang <112304638+alexjoo-kang@users.noreply.github.com>
2025-04-11 00:29:18 +0900

    Update setup.py_comma
    omitted comma after the "[macOS-arm64]"

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "Environment :: MacOS X",
     ],
     python_requires=">=3.10",
-    platforms=["macOS-arm64"]
+    platforms=["macOS-arm64"],
     entry_points={
         "console_scripts": [
             "proteinpropertiespredictor=protein_properties_predictor.cli:main",  # CLI-based execution only
